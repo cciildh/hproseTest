41b40cc1ccaecba095ba0cff28a9812f8db40793
ldh <493830938@qq.com>
2021-03-28 18:22:36 +0800

    异步存储日志

---
+++ asyncTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace asyncTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("开始办正事！");
+            var sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(200);
+                sum++;
+            }
+            Console.WriteLine("开始写日志！");
+            logUtils.SaveLog(sum.ToString());
+
+            Console.WriteLine("正事办理完成！");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ asyncTest/logUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncTest
+{
+    public class logUtils
+    {
+        public  static async  void SaveLog( string result )
+        {
+           await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("日志写完了"+result);
+            });
+
+        }
+
+    }
+}
